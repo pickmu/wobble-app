3f304c0dea7b29fe881e20ae233ef0a98a0b5f79
Mazen El Ali <mazenelali>
2023-12-06 12:15:24 +0200

    translate

--- app/Screens/UserScreens/TripHistory.js
+++ app/Screens/UserScreens/TripHistory.js
@@ -4,13 +4,16 @@ import { StyleSheet } from "react-native";
 import TripCard from "../../Components/TripCard";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { i18nStore } from "../../MobX/I18nStore";
+import { authStore } from "../../MobX/AuthStore";
 
 const TripHistory = () => {
+    const { i18n } = i18nStore;
     const [dataHistory, setDataHistory] = useState(null);
 
     useEffect(() => {
         axios
-            .get(`${process.env.EXPO_PUBLIC_API_URL}order/getOrder`)
+            .get(`${process.env.EXPO_PUBLIC_API_URL}userInfo/tripHistory/${authStore.userInfo._id}`)
             .then((response) => {
                 setDataHistory(response.data);
             })
@@ -23,7 +26,7 @@ const TripHistory = () => {
     if (dataHistory.length == 0) return <Text className="w-full p-5 text-gray-500 text-center text-l"> No History Yet </Text>
     return (
         <View>
-            <Text className="px-5 py-3 text-3xl font-bold">My Rides </Text>
+            <Text className="px-5 py-3 text-3xl font-bold"> {`${i18n.t("tripHistory.title")}`} </Text>
             <ScrollView className=" w-full p-5 h-full">
                 {dataHistory.map((history, index) => (
                     <TripCard key={index} {...history} />
