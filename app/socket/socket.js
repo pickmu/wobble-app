00189f4fd12cd760481604b229da9ed22e4c869c
Mazen El Ali <mazenelali>
2024-02-12 15:45:55 +0200

    setup chat

---
+++ app/socket/socket.js
@@ -0,0 +1,44 @@
+// socket.js (Socket module)
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+
+
+const Socket = io("http://localhost:5000");
+
+export {Socket};
+
+// useSocket.js (Custom Hook)
+
+
+function useSocket() {
+  const [isConnected, setIsConnected] = useState(Socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents((previous) => [...previous, value]);
+    }
+
+    Socket.on("connect", onConnect);
+    Socket.on("disconnect", onDisconnect);
+    Socket.on("foo", onFooEvent);
+
+    return () => {
+      Socket.off("connect", onConnect);
+      Socket.off("disconnect", onDisconnect);
+      Socket.off("foo", onFooEvent);
+    };
+  }, []);
+
+  return { isConnected, fooEvents,Socket  };
+}
+
+export { useSocket};
