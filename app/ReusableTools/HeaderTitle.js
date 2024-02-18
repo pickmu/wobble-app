a12c616fa00596b24f28ceb3e13b893fefb733ef
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2024-02-18 19:39:51 +0200

    useSafeAreaInsets instead of safeare

--- app/ReusableTools/HeaderTitle.js
+++ app/ReusableTools/HeaderTitle.js
@@ -1,38 +1,37 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useNavigation, DrawerActions } from "@react-navigation/native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HeaderTitle = ({ title, isDrawer, isChat, cancel }) => {
   const navigation = useNavigation();
 
-  return (
-    <>
-      <SafeAreaView />
-      <View className="w-[50%]">
-        <TouchableOpacity
-          onPress={
-            () =>
-              isChat
-                ? navigation.navigate("Map") // Navigate to the Home screen if isChat is true
-                : isDrawer
-                ? navigation.dispatch(DrawerActions.openDrawer())
-                : navigation.goBack() // Otherwise, go back
-          }
-        >
-          <Image
-            source={require("../Images/Icons/arrow.png")}
-            className="w-[50px] h-[20px] mb-3 self-center"
-          />
-        </TouchableOpacity>
+  const insets = useSafeAreaInsets();
 
-        <View
-          className="bg-headers rounded-r-full py-5 items-center"
-          style={{ width: cancel ? "150%" : "100%" }}
-        >
-          <Text className="text-[25px] font-bold">{title}</Text>
-        </View>
+  return (
+    <View className="w-[50%]" style={{ paddingTop: insets.top }}>
+      <TouchableOpacity
+        onPress={
+          () =>
+            isChat
+              ? navigation.navigate("Map") // Navigate to the Home screen if isChat is true
+              : isDrawer
+              ? navigation.dispatch(DrawerActions.openDrawer())
+              : navigation.goBack() // Otherwise, go back
+        }
+      >
+        <Image
+          source={require("../Images/Icons/arrow.png")}
+          className="w-[50px] h-[20px] mb-3 self-center"
+        />
+      </TouchableOpacity>
+
+      <View
+        className="bg-headers rounded-r-full py-5 items-center"
+        style={{ width: cancel ? "150%" : "100%" }}
+      >
+        <Text className="text-[25px] font-bold">{title}</Text>
       </View>
-    </>
+    </View>
   );
 };
 
